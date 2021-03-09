2d48615218e70845c95c3c97ab4b0129cb674bcd
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-03-09 13:28:33 +0200

    blob binding, axios request to weather API

--- bicycle-weather-01/index.js
+++ bicycle-weather-01/index.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
+const axios = require('axios');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // Add a message to the Storage queue,
-    // which is the name passed to the function.
-    // context.bindings.msg = (req.query.name || req.body.name);
-
+    const cityName = (req.query.cityName || (req.body && req.body.cityName));
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        // body: responseMessage
-        body: process.env.TESTI2
+        body: response.data
     };
 }
