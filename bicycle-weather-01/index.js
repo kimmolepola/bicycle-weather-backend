098da0f68802fb6f03b0aa6313479d7367457caa
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-03-09 19:56:40 +0200

    env template

--- bicycle-weather-01/index.js
+++ bicycle-weather-01/index.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 
 module.exports = async function (context, req) {
     const cityName = (req.query.cityName || (req.body && req.body.cityName));
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+    console.log("http request, cityName: ", cityName);
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`)
     context.res = {
         body: response.data
     };
